c60278ae491e505bdc14401f6d8466ed10f13271
NicolePozo <nicolepozo.salas@gmail.com>
2021-07-19 15:12:51 -0400

    version iphone

--- src/lib/signUp.js
+++ src/lib/signUp.js
@@ -16,11 +16,6 @@ export const signUpView = () => {
   inputEmailUp.setAttribute('type', 'email');
   inputEmailUp.setAttribute('placeholder', 'Correo electrónico');
   mainUp.appendChild(inputEmailUp);
-  /*const imgUp=document.createElement("img");
-  imgUp.setAttribute("id", "imgUp");
-  imgUp.src="./lib/assets/palillohombre.jpg";
-  mainUp.appendChild(imgUp);*/
-
   const inputPasswordUp = document.createElement('input');
   inputPasswordUp.setAttribute('id', 'passwordUp');
   inputPasswordUp.setAttribute('type', 'password');
@@ -49,10 +44,12 @@ export const signUpView = () => {
   buttonToGoogleUp.addEventListener('click', googleAccess);
   mainUp.appendChild(buttonToGoogleUp);
   containerUp.appendChild(mainUp);
+  const divImgUp=document.createElement("div");
+  divImgUp.setAttribute("id","divImgUp");
   const imgUp=document.createElement("img");
   imgUp.setAttribute("id", "imgUp");
-  imgUp.src="./lib/assets/palillohombre.jpg";
-  containerUp.appendChild(imgUp);
+  divImgUp.appendChild(imgUp);
+  containerUp.appendChild(divImgUp);
   const footerUp = document.createElement('footer');
   footerUp.setAttribute('id', 'footerUp');
   footerUp.innerHTML = 'Derechos reservados @Tejer&Punto';
