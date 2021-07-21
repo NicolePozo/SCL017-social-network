7e02d91138928e1dd5aa8cdc7efd5b89e61ea8c7
NicolePozo <nicolepozo.salas@gmail.com>
2021-07-21 12:11:00 -0400

    carrusel

--- src/lib/signUp.js
+++ src/lib/signUp.js
@@ -44,15 +44,76 @@ export const signUpView = () => {
   buttonToGoogleUp.addEventListener('click', googleAccess);
   mainUp.appendChild(buttonToGoogleUp);
   containerUp.appendChild(mainUp);
-  const divImgUp=document.createElement("div");
+  const divImgUp=document.createElement("div"); //slideshow
   divImgUp.setAttribute("id","divImgUp");
+  divImgUp.setAttribute("class", "slideshow-container");
+  const divCarruselOne=document.createElement("div");
+  divCarruselOne.setAttribute("class","mySlides fade") //divs mySliders
   const imgUp=document.createElement("img");
-  imgUp.setAttribute("id", "imgUp");
-  divImgUp.appendChild(imgUp);
+  imgUp.setAttribute("id", "imgUpOne");
+  imgUp.setAttribute("src","./lib/assets/palillohombre.jpg");
+  divImgUp.appendChild(divCarruselOne);
+  divCarruselOne.appendChild(imgUp);
+  const divCarruselTwo=document.createElement("div");
+  divCarruselTwo.setAttribute("class","mySlides fade");
+  const imgUpTwo=document.createElement("img");
+  imgUpTwo.setAttribute("src","./lib/assets/img2.jpg");
+  imgUpTwo.setAttribute("id","imgUpTwo");
+  divImgUp.appendChild(divCarruselTwo);
+  divCarruselTwo.appendChild(imgUpTwo);
+  const divCarruselThree=document.createElement("div");
+  divCarruselThree.setAttribute("class","mySlides fade");
+  const imgUpThree=document.createElement("img");
+  imgUpThree.setAttribute("src","./lib/assets/crochetmujer.jpg");
+  imgUpThree.setAttribute("id","imgUpThree");
+  divImgUp.appendChild(divCarruselThree);
+  divCarruselThree.appendChild(imgUpThree);
+  const divDots=document.createElement("div");  //creando puntos
+  divDots.setAttribute("style", "text-align:center");
+  divDots.setAttribute("id","divDots");
+  const spanDotsOne=document.createElement("span");
+  spanDotsOne.setAttribute("class","dots");
+  divDots.appendChild(spanDotsOne);
+  const spanDotsTwo=document.createElement("span");
+  spanDotsTwo.setAttribute("class","dots");
+  divDots.appendChild(spanDotsTwo);
+  const spanDotsThree=document.createElement("span");
+  spanDotsThree.setAttribute("class","dots");
+  divDots.appendChild(spanDotsThree);
+  containerUp.appendChild(divDots);
   containerUp.appendChild(divImgUp);
   const footerUp = document.createElement('footer');
   footerUp.setAttribute('id', 'footerUp');
   footerUp.innerHTML = 'Derechos reservados @Tejer&Punto';
   containerUp.appendChild(footerUp);
+
+  //carrusel//
+var slideIndex = 0;
+showSlides();
   return containerUp;
+
+
+
+
+function showSlides() {
+  let i;
+  let slides = containerUp.getElementsByClassName("mySlides");
+  let dots = containerUp.getElementsByClassName("dots");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  console.log(slides)
+  console.log(dots)
+  console.log(slideIndex)
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+
+}
+
 };
